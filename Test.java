5069686e04e6d8766ebe748f496882cd317f5d49
GinaAnais <gina.medina@estudiante.ucsm.edu.pe>
2025-05-19 08:29:02 -0500

    Ejercicio 2 del laboratorio 6

--- Test.java
+++ Test.java
@@ -1,20 +1,25 @@
-package Ejercicio1;
+package Ejercicio2;
 
 import Actividad1.ExceptionIsEmpty;
 
 public class Test {
 
-	public static void main (String[] args) throws ExceptionIsEmpty {
-		StackLink<Integer> stackLinkInt = new StackLink<Integer>(4);
-		stackLinkInt.push(34);
-		stackLinkInt.push(-100);
-		stackLinkInt.push(100);
-		stackLinkInt.push(54);
-		stackLinkInt.push(0);
-		System.out.println(stackLinkInt);
-		System.out.println("En la cabeza se encuentra el numero: " + stackLinkInt.top());
-		System.out.println("Sacamos el elemento: " + stackLinkInt.pop());
-		System.out.println(stackLinkInt);
-		System.out.println("La COLA se encuentra vacia? " + stackLinkInt.isEmpty());
+	public static void main(String[] args) throws ExceptionIsEmpty {
+		QueueArray<Integer> queueArray = new QueueArray<Integer>(5);
+		queueArray.enqueue(45);
+		queueArray.enqueue(100);
+		queueArray.enqueue(00);
+		queueArray.enqueue(80);
+		queueArray.enqueue(-112);
+		System.out.println(queueArray);
+		queueArray.dequeue();
+		System.out.println(queueArray);
+		queueArray.enqueue(-110);
+		queueArray.dequeue();
+		queueArray.dequeue();
+		System.out.println(queueArray);
+		System.out.println("A la cabeza de la COLA se encuentra el elemento: " + queueArray.front());
+		System.out.println("Al final de la COLA se encuentra el elemento: " + queueArray.back());
 	}
+
 }
