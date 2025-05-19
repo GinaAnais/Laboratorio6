ea610c49defbb5f9a12d2a965ab69b744cbf99a7
GinaAnais <gina.medina@estudiante.ucsm.edu.pe>
2025-05-19 08:29:43 -0500

    Ejercicio 3 del laboratorio 6

--- Test.java
+++ Test.java
@@ -1,25 +1,18 @@
-package Ejercicio2;
+package Ejercicio3;
 
 import Actividad1.ExceptionIsEmpty;
 
 public class Test {
 
-	public static void main(String[] args) throws ExceptionIsEmpty {
-		QueueArray<Integer> queueArray = new QueueArray<Integer>(5);
-		queueArray.enqueue(45);
-		queueArray.enqueue(100);
-		queueArray.enqueue(00);
-		queueArray.enqueue(80);
-		queueArray.enqueue(-112);
-		System.out.println(queueArray);
-		queueArray.dequeue();
-		System.out.println(queueArray);
-		queueArray.enqueue(-110);
-		queueArray.dequeue();
-		queueArray.dequeue();
-		System.out.println(queueArray);
-		System.out.println("A la cabeza de la COLA se encuentra el elemento: " + queueArray.front());
-		System.out.println("Al final de la COLA se encuentra el elemento: " + queueArray.back());
+	public static void main (String[] args) throws ExceptionIsEmpty {
+		PriorityQueueLinked<Integer, Integer> prioQueueLink = new PriorityQueueLinked<Integer, Integer>(5);
+		prioQueueLink.enqueue(34, 2);
+		prioQueueLink.enqueue(0, 2);
+		prioQueueLink.enqueue(2, 1);
+		System.out.println(prioQueueLink);
+		prioQueueLink.dequeue();
+		System.out.println(prioQueueLink);
+		
+		System.out.println("Al final se encuentra el elemento: " + prioQueueLink.back());
 	}
-
 }
